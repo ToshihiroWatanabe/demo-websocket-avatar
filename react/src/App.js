84408ab89b7ac2ade4b7b5ca55131e50176dcc83
ToshihiroWatanabe <79039863+ToshihiroWatanabe@users.noreply.github.com>
2021-06-03 15:08:38 +0900

    update: 領域内をドラッグ移動可能なアバターアイコンを設置

--- react/src/App.js
+++ react/src/App.js
@@ -1,24 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import { Avatar } from "@material-ui/core";
+import { makeStyles } from "@material-ui/core/styles";
+import { Rnd } from "react-rnd";
+
+const sandBoxWidth = 320;
+const sandBoxHeight = 320;
+
+const sandBoxLeft = 32;
+const sandBoxTop = 32;
+
+let avatarX = Math.floor(Math.random() * sandBoxWidth);
+let avatarY = Math.floor(Math.random() * sandBoxHeight);
+
+const useStyles = makeStyles(() => ({
+  sandBox: {
+    position: "absolute",
+    width: sandBoxWidth,
+    height: sandBoxHeight,
+    left: sandBoxLeft,
+    top: sandBoxTop,
+    border: "solid 2px #000",
+    backgroundImage:
+      "linear-gradient(0deg, transparent 31px, #333 32px), linear-gradient(90deg, transparent 31px, #333 32px)",
+    backgroundColor: "#ddd",
+    backgroundSize: "32px 32px",
+  },
+  myAvatar: {
+    position: "absolute",
+    transform: "translate(-50%, -50%)",
+  },
+}));
 
 function App() {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <>
+      <div className={classes.sandBox}>
+        <Rnd
+          default={{
+            x: avatarX,
+            y: avatarY,
+            width: 0,
+            height: 0,
+          }}
+          bounds="parent"
+          enableResizing={{
+            top: false,
+            right: false,
+            bottom: false,
+            left: false,
+            topRight: false,
+            bottomRight: false,
+            bottomLeft: false,
+            topLeft: false,
+          }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Avatar className={classes.myAvatar}></Avatar>
+        </Rnd>
+      </div>
+    </>
   );
 }
 
